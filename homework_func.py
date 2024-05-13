4e5e3b1e656b38f44adc700bc0eab0f6b3937322
Sapphirenatalie <168541642+Sapphirenatalie@users.noreply.github.com>
2024-05-13 10:51:24 +0300

    Update homework_func.py

--- homework_func.py
+++ homework_func.py
@@ -1,30 +1,13 @@
-def print_params(day_of_week):
-    print('Today is:', day_of_week)
-
-
-print_params('Monday')
-print_params('Tuesday')
-print_params('Wednesday')
-print_params('Thursday')
-print_params('Friday')
-print_params('Saturday')
-print_params('Sunday')
-
-print_params('Monday')
-print_params('Tuesday')
-print_params('Wednesday')
-print_params('Thursday')
-print_params('Friday')
-print_params('Saturday')
-print_params('Sunday')
-
-print('---------------------')
 #последовательный вывод дней недели функция плюс цикл for:
 def print_params(day_of_week):
     day_of_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
     for i in day_of_week:
         print('Today is ' + i)
+    print()
 
+
+print_params(day_of_week='i')
+print_params(day_of_week='i')
 
 print_params(day_of_week='i')
 print_params(day_of_week='i')

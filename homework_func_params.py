e2234ad942591a5392c038f88bf254697438596c
Sapphirenatalie <168541642+Sapphirenatalie@users.noreply.github.com>
2024-05-18 18:03:17 +0300

    Update homework_func_params.py

--- homework_func_params.py
+++ homework_func_params.py
@@ -16,10 +16,6 @@ values_list = [7, False, [3, 8]]
 values_dict = {'a': 4, 'b': 5, 'c': 6}
 
 
-def print_params(a, b, c):
-    print(a, b, c)
-
-
 print_params(*values_list)
 print_params(**values_dict)
 
@@ -27,10 +23,6 @@ print()
 print('Распаковка + отдельные параметры:')
 
 values_list_2 = ['Санкт-Петербург', 1703]
-
-
-def print_params(a, b, c):
-    print(a, b, c)
 
 
 print_params(42, *values_list_2)

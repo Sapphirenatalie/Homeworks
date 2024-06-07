49c1d7ce956f4edfffe54a31d3df08e4c7aaf0fd
Sapphirenatalie <168541642+Sapphirenatalie@users.noreply.github.com>
2024-06-07 12:29:55 +0300

    Add files via upload

---
+++ module_5_2.py
@@ -0,0 +1,15 @@
+class House:
+
+    def __init__(self, name, floors):
+        self.numberOfFloors = floors
+        print('исходное кол-во этажей -', self.numberOfFloors)
+
+    def __setNewNumberOfFloors__(self, floors):
+        self.numberOfFloors = floors
+        print('измененное кол-во этажей -', self.numberOfFloors)
+
+
+house_1 = House('Многоквартирный дом', 10)
+house_1.__setNewNumberOfFloors__(floors=8)
+print()
+house_1.__setNewNumberOfFloors__(int(input('Введите необходимое кол-во этажей: ')))

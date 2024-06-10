55e67fe381ef1ce357dcf48f850324bec1ff203f
Sapphirenatalie <168541642+Sapphirenatalie@users.noreply.github.com>
2024-06-10 18:56:56 +0300

    Add files via upload

---
+++ module_5_3.py
@@ -0,0 +1,31 @@
+
+
+class Building:
+
+    def __init__(self, floors, types):
+        self.numberOfFloors = floors
+        self.buildingType = types
+        print('кол-во этажей -', self.numberOfFloors, ', тип обЪекта - ', self.buildingType)
+
+    def __eq__(self, other):
+        return self.numberOfFloors == other.numberOfFloors and self.buildingType == other.buildingType
+
+
+house_1 = Building(10, 'панельный дом')
+house_2 = Building(5, 'кирпичный дом')
+print()
+
+print(house_1 == house_2)
+if house_1 == house_2:
+    print('дома одинаковые')
+else:
+    print('дома различаются')
+print()
+house_2.buildingType = 'панельный дом'
+house_2.numberOfFloors = 10
+print(house_1 == house_2)
+if house_1 == house_2:
+    print('дома одинаковые')
+else:
+    print('дома различаются')
+

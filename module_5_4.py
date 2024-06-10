6c7724ba58b25bd7db393333020d1db21b1adfc0
Sapphirenatalie <168541642+Sapphirenatalie@users.noreply.github.com>
2024-06-11 01:55:54 +0300

    Add files via upload

---
+++ module_5_4.py
@@ -0,0 +1,14 @@
+class Building:
+    total = 0
+
+    def __init__(self, quantity, floors, types):
+        self.numberOfFloors = floors
+        self.buildingType = types
+        Building.total = quantity
+
+        for i in range(1, Building.total + 1):
+            if i <= self.total:
+                print(f'Здание № {i}\n{self.buildingType}\n{self.numberOfFloors} этажей \n')
+
+
+house = Building(40, 7, 'кирпичный дом')
